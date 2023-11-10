dd64276133423fad64e43877beb78e7cf684274f
quan.nguyen <qu.nguyen@reply.de>
2023-11-10 11:37:37 +0100

    init

---
+++ src/main/java/com/fincon/techtalk/example/techtalkexample/TechtalkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.fincon.techtalk.example.techtalkexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtalkExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechtalkExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fincon/techtalk/example/techtalkexample/TechtalkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fincon.techtalk.example.techtalkexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechtalkExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
